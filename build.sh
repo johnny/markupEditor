14a37cc255bc615d5f3c1d0194c0895e10de1c49
johnny <jvadev@gmail.com>
2011-08-23 21:26:40 +0200

    update build

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ git checkout gh-pages
 rm -R javascripts/ stylesheets/
 mv build/* .
 mv ../README.mkd .
+git add .
 git commit -am 'Update pages. See main branch for changes'
 git push origin gh-pages
 git checkout master
