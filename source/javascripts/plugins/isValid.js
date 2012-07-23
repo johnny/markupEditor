784fc5071468eff59b2e715626eed21c95b23eaa
Jonas von Andrian <jvadev@gmail.com>
2012-07-24 00:10:52 +0200

    indentation

--- source/javascripts/plugins/isValid.js
+++ source/javascripts/plugins/isValid.js
@@ -39,12 +39,12 @@
         //Check if it's empty or an invalid email
         if(fieldValue === '') {
           errorMsg = _('errorText') || opts.errorText;
-	  valid = false;
+	        valid = false;
         } else if($input.hasClass('email')) {
-	  if(!(/^([_a-z0-9-]+)(\.[_a-z0-9-]+)*@([a-z0-9-]+)(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/.test(fieldValue))) {
-	    errorMsg = _('emailErrorText') || opts.emailErrorText;
-	    valid = false;
-	  }
+	        if(!(/^([_a-z0-9-]+)(\.[_a-z0-9-]+)*@([a-z0-9-]+)(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/.test(fieldValue))) {
+	          errorMsg = _('emailErrorText') || opts.emailErrorText;
+	          valid = false;
+	        }
         }
         
         //If there is an error, display it
