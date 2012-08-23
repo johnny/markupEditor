67ac584e3afea54ab01ec0a0632b84a4905635e6
Jonas von Andrian <jvadev@gmail.com>
2012-08-23 12:25:49 +0200

    fixed links example

--- source/js/modes/textile/textileMode.js
+++ source/js/modes/textile/textileMode.js
@@ -410,9 +410,35 @@
         }
       }
 
+      html = html.replace(/\s*<img[^>]*>\s*/gi, function(match){
+        var img = $(match),
+        replacement = img.attr('src'),
+        title = img.attr('title');
+
+        if(title && !/^\s*$/.test(title)){
+          replacement = replacement + "(" + title + ")";
+        }
+        return "!" + replacement + "!";
+      });
+
+      html = html.replace(/(\s*)<a href="([^\"]*)">((?:.|[\r\n])*?)<\/a>(\s*)/gi, function(match, startSpace, uri, content, endSpace){
+        var out = startSpace ? ' ' : '';
+
+        if(/^\s*![^!]+!\s*$/.test(content)){
+          out += $.trim(content) + ":";
+        } else {
+          out += "\"" + content + "\":";
+        }
+        out += uri;
+        if(endSpace){
+          out += ' ';
+        }
+        return out;
+      });
+
       html = html.replace(/\s*<(ul|ol)>((.|[\r\n])*?)<\/\1>\s*/gi, function(match, tag, items){
         var bullet = tag == 'ul' ? '*' : '#';
-        
+
         eachRegexp(['b','i', 'u', 'del'], function(regexp, delimiter){
           items = items.replace(regexp, function(match, startSpace, text, endSpace){
             startSpace = startSpace ? ' ' : '';
@@ -420,10 +446,9 @@
             return startSpace + delimiter + text + delimiter + endSpace;
           });
         });
-
         return items.replace(/\s*<li>((.|[\r\n])*?)<\/li>\s*/gi, bullet + " $1\n") + "\n";
       });
-      
+
       html = html.replace(/ *<(p|h[1-4])([^>]*)>((.|[\r\n])*?)<\/\1>\s*/gi, function(match, tag, attributes, content){
         var front = "", cssClass = attributes.match(/class=\"([^"]*)/);
         if(cssClass){
@@ -442,30 +467,6 @@
         return front + content.replace(/<br ?\/?>\s*/gi, "\n") + "\n\n";
       });
       
-      html = html.replace(/\s*<img[^>]*>\s*/gi, function(match){
-        var img = $(match),
-        replacement = img.attr('src'),
-        title = img.attr('title');
-
-        if(title && !/^\s*$/.test(title)){
-          replacement = replacement + "(" + title + ")";
-        }
-        return "!" + replacement + "!";
-      });
-      html = html.replace(/(\s*)<a href="([^\"]*)">((?:.|[\r\n])*?)<\/a>(\s*)/gi, function(match, startSpace, uri, content, endSpace){
-        var out = startSpace ? ' ' : '';
-
-        if(/^\s*![^!]+!\s*$/.test(content)){
-          out += $.trim(content) + ":";
-        } else {
-          out += "\"" + content + "\":";
-        }
-        out += uri;
-        if(endSpace){
-          out += ' ';
-        }
-        return out;
-      });
       html = html.replace(/\s*<code[^>]*>((.|[\r\n])*?)<\/code>\s*/gi, ' @$1@ ');
       html = html.replace(/(\r\n|\n){3,}/g, "\n\n");
       html = html.replace(/&nbsp;/g, ' ');
--- source/js/test/textileMode.js
+++ source/js/test/textileMode.js
@@ -402,6 +402,8 @@ $(document).ready(function(){
       .match("h1. paragraph\n\n* item1\n\nh1. paragraph");
   });
 
+  module('textileConversion')
+
   function checkConversion(value, result){
     textile.set(value)
       .click(".wysiwyg")
@@ -418,18 +420,19 @@ $(document).ready(function(){
     checkConversion(" *bold* ");
     checkConversion("_italic_");
     checkConversion(" _italic_ ");
-    checkConversion("\"This\":uri", "\"This\":uri ");
+    checkConversion("\"This\":uri");
     checkConversion(" \"This\":uri ");
     checkConversion("!src!");
-    checkConversion("!src!:uri", "!src!:uri ");
+    checkConversion("!src!:uri");
     checkConversion("!src(Title)!");
-    checkConversion("!src(Title)!:uri", "!src(Title)!:uri ");
+    checkConversion("!src(Title)!:uri");
     checkConversion("* _test_\n* *test*\n\n* test");
     checkConversion("* test\n# test", "* test\n\n# test");
     checkConversion("* _test<br>test_\n* test");
     checkConversion("&nbsp;", " ");
     checkConversion("* list\n\nh1. heading");
     checkConversion("_test<br>test_", "_test_\n_test_");
+    checkConversion("* \"link\":uri\n* \"link2\":uri2")
   });
 
   function checkConversionFromHTML(html, result){
